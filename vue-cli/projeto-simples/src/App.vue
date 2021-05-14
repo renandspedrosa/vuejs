c323938cd3fa841f50554e129618044eff34f612
Renan Pedrosa <renandspedrosa@gmail.com>
2021-05-13 21:52:13 -0300

    install vue + plugins

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{titulo}}
+    <button @click="titulo+='!'">alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+      return {
+        titulo:'teste data usando vue JS'
+      }
+    }
+}
+</script>
+
+<style>
+#app{
+  background-color: chocolate;
+  color: #fff;
+}
+</style>
